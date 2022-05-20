cd762561cc7901b1f6573bb15a4f9cc258313a9a
LuisEncastin <luisencastin@gmail.com>
2022-05-20 17:33:03 -0500

    Se termina el proyecto con requerimientos minimos

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,7 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+
+    var myLatLng = {lat: 19.27652230649518, lng: -99.673056025033};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +11,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: ' Despacho Cipla Abogados' // Title Location
     });
 }
